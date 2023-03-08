f0aba5b0d459a9922dbf88faa104b0dd2efc0c39
leticia.vargas <le.mvargas@gmail.com>
2023-03-08 18:49:59 -0300

    Update board components

--- front/src/components/Board/Collumn.jsx
+++ front/src/components/Board/Collumn.jsx
@@ -1,9 +1,30 @@
-import { Card } from 'components';
 
-function Collumn({ title }) {
+import { Card, Button } from 'components';
+import useCards from 'hooks/useCards';
+
+function Collumn({ title, actionButtonTitle, actionButton }) {  
+/*  const { getCardList, postCard } = useCards();
+
+  const teste = postCard({
+    titulo: "titulo do card",
+    conteudo: "conteudo de um card"
+  })
+
+  console.log('TESTE >>>', teste)
+
+
+  getCardList();
+*/
   return(
     <div className="collumn">
-      <header>{title}</header>
+      <header>
+        <h2>{title}</h2>
+        { actionButtonTitle && 
+          <Button className="square" onClick={actionButton}>
+            {actionButtonTitle}
+          </Button>
+        }
+      </header>
       <main>
         <Card />
         <Card />
--- front/src/components/Board/index.jsx
+++ front/src/components/Board/index.jsx
@@ -1,10 +1,18 @@
 import Collumn from "./Collumn";
+import { FaPlusCircle } from "react-icons/fa";
 import './styles.scss';
 
-function Board() {
+function Board({ handleNewCard }) {
+
+  
+
   return(
     <main className="board">
-      <Collumn title="To Do" />
+      <Collumn 
+        title="To Do" 
+        actionButtonTitle={"Novo card"}
+        actionButton={handleNewCard}
+      />
       <Collumn title="Doing" />
       <Collumn title="Done" />
     </main>
